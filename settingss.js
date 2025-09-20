e7c7e888a05503852b513b9d4c3b532ef7552e32
yousufjani921-cloud <yousufjani921@gmail.com>
2025-09-20 15:38:54 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
-  OWNER_NUMBER: "", // put your phone number here
+  SESSION_ID: "malvin~hjklyJAY#F2eRTRNdiaTW2sRNgLSsMwtLeb15pC1SHYwstZVL3d4",  // add your Session Id here
+  OWNER_NUMBER: "923097117856", // put your phone number here
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   TIMEZONE: "Africa/Kampala" //put your country timeZone....leave blank if u don't know.
 };
